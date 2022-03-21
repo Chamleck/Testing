d9ba60d876fc7d30f9a636f816384d9d283cb020
Chamleck <chamlecks@gmail.com>
2022-03-21 13:32:56 +0200

    Удалил переменные

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,11 +3,9 @@ package ua.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        String somebody = "world!";
-        System.out.println("Hello" + somebody);
 
-        double l = 8.0;
-        double s = l*l;
-        System.out.println("полощадь квадрата со стороной" + l + "=" + s);
+        System.out.println("Hello World!" );
+
+       
     }
 }
